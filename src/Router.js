d050435b5475828742fbc1a7e4bbc10513a292b9
sukru <sukrugundogdudatascience@gmail.com>
2020-11-04 22:13:02 +0300

    started example

---
+++ src/Router.js
@@ -0,0 +1,22 @@
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Restaurants, Favorites } from "./pages"
+
+const Tab = createBottomTabNavigator();
+
+function Router() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="FavoritesPage" component={Favorites} />
+        <Tab.Screen name="RestaurantPage" component={Restaurants} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
