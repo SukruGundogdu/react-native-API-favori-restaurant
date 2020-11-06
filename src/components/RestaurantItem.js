0cf0fe1270c3bda8f9bcb05a8ffb62fd93fbbd02
sukru <sukrugundogdudatascience@gmail.com>
2020-11-06 14:03:45 +0300

    Created reducer and store, add buton for restaurants

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -3,9 +3,11 @@ import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 const RestaurantItem = ({ item }) => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>{item.name}</Text>
-        </View>
+        <TouchableOpacity>    
+            <View style={styles.container}>
+                <Text style={styles.text}>{item.name}</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
 
---
+++ src/context/reducer.js
@@ -0,0 +1,22 @@
+export function reducer(state, action){
+    switch(action,type){
+        case "ADD_TO_FAVORITE":
+            // 1)const newList=[...state.favoriteList];
+            // 1)newList.push(action.payload.selectedRestaurant);
+            // 1)state.favoriteList = newList;
+            // 1)return {...state}
+
+            // 2)const newList = [...state.favoriteList, action.payload.selectedRestaurant]
+            // 2)return {...state, favoriteList: newList}
+
+            // 3)
+            return { 
+            ...state, 
+            favoriteList: [...state.favoriteList, action.payload.selectedRestaurant] 
+            }
+
+        default:
+            return state;
+
+    }
+}
