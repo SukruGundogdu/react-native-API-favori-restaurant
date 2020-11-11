684285424d4e80557c3ba929e1948d1216e562d7
Sukru Gundogdu <sukrugundogdudatascience@gmail.com>
2020-11-11 19:10:31 +0300

    styling favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -7,10 +7,11 @@ const Favorites = (props) => {
     return (
         <SafeAreaView style={{flex:1}} >
             <View style={{flex:1}} >
+            <Text style={{fontSize:25, textAlign:"center", fontWeight:"bold"}} >Favori Restoranlar</Text>
                 <FlatList
                     keyExtractor={(_, index) => index.toString()}
                     data={favList}
-                    renderItem={({item}) => <Text>{item.name}</Text>}
+                    renderItem={({item}) => <Text style={{backgroundColor:'#a7b6bd', margin:10, padding: 10, borderRadius:10}}><Icon name={"heart"} size={20} color={"yellow"} /> {item.name}</Text>}
                     ListEmptyComponent={() => <Text>Notig Favorite</Text>}
                 />
             </View>
